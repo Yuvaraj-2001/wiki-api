956c514e1d0143f5855689a4769e4ee04163cdcb
Yuvaraj-2001 <yuvayuvaraj720444@gmail.com>
2023-02-18 00:41:28 +0530

    pitch pro

--- app.js
+++ app.js
@@ -20,7 +20,16 @@ const articleSchema = {
   title: String,
   content: String
 };
+const pitchProContactSchema = {
+  email: String,
+  message: String,
+  phone: String,
+  service: String,
+  state: String,
+  username: String,
+}
 
+const Pitch = mongoose.model("pitchs", pitchProContactSchema);
 const Article = mongoose.model("articles", articleSchema);
 
 /////////////////////////All Articles///////////////////////////////////
@@ -64,6 +73,57 @@ app.route("/article").post(function(req, res){
 });
 
 
+///////////////////////// Pitch PRO ////////////////
+app.route("/pitchpro")
+.get(function(req, res){
+  Pitch.find(function(err, articles){
+    if (articles.length === 0) {
+      res.status(404);
+      res.send("No Pitches are Present, Please add one");
+     
+    } else if(articles) {
+      const jsonArticles = JSON.stringify(articles);
+      res.send(jsonArticles);
+    }else{
+      res.status(400);
+      res.send("OOPS something crahes" + err);
+    }
+  });
+})
+.post(function(req, res){
+  const newPitch = Pitch({
+    email: req.body.email,
+    message: req.body.message,
+    phone: req.body.phone,
+    service: req.body.service,
+    state: req.body.state,
+    username: req.body.username
+  });
+  console.log(req.body);
+  if( Object.keys(req.body).length === 0){
+    res.status(400);
+    res.send("There is nothing in body");
+    return
+  }
+  newPitch.save(function(err){
+    if (!err){
+      res.send("Successfully added a new Pitch.");
+    } else {
+      res.status = 400;
+      res.send(err);
+    }
+  });
+})
+.delete(function(req, res){
+  Pitch.deleteMany(function(err){
+      if (!err){
+      res.send("Successfully deleted all the Entries in Pitch DB.");
+      } else {
+      res.status = 500;
+      res.send(err);
+      }
+  });
+});
 /////////////////////////Individual Articles///////////////////////////////////
 
 app.route("/articles/:articleTitle")
